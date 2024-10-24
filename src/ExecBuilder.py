eae9341f9969edbe3e55e621d4ac9e759efff7aa
LuisDFJ <luis.fuju@outlook.com>
2024-10-24 09:34:18 -0600

    Added command to delete .rst .rdb and .db results after each experiment.

--- src/ExecBuilder.py
+++ src/ExecBuilder.py
@@ -6,3 +6,6 @@ def execBuilder( path : str ):
             print( f"echo Running {dir}", file=pFile )
             print( "ANSYS241.exe", end=" ", file=pFile )
             print( f"-b nolist -dir {dir}/res -j results -i {dir}/program.ansys -o {dir}/o.log", file=pFile, end="\n\n" )
+            print( f"del /S {dir}/res/*.rst", file=pFile )
+            print( f"del /S {dir}/res/*.rdb", file=pFile )
+            print( f"del /S {dir}/res/*.db" , file=pFile , end="\n\n" )
