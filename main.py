c5db87d4060ccec610f042b964c10590197653c6
LuisDFJ <luis.fuju@outlook.com>
2024-10-24 09:10:07 -0600

    Ready for test.

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from src.ExecBuilder import execBuilder
 
 SIMTYPE     = "shell"
 L           = [ (0.010,1) ]
-THICKNESS   = [ 0.0006 ] # , 0.0005, 0.0004 ]
-LAYERS      = [ 2 ]
-TOPOLOGY    = [ ("gyroid",5) ]#, [ ("diamond",2), ("gyroid",5) ]      
+THICKNESS   = [ 0.0006 , 0.0005, 0.0004, 0.0003 ]
+LAYERS      = [ 4,6,8,10 ]
+TOPOLOGY    = [ ("diamond",2), ("gyroid",5) ]      
 SECTION     = { 
-    #"Rectangular" : [ 0.021 ],#, 0.021, 0.020, 0.019, 0.018 ],
-    "Circular"    : [ 0.011 ]#, 0.0105,0.010, 0.0095,0.009 ]
+    "Rectangular" : [ 0.022, 0.021, 0.020, 0.019, 0.018 ],
+    "Circular"    : [ 0.011, 0.0105,0.010, 0.0095,0.009 ]
 }
 
 if __name__ == "__main__":
     c = 1
-    #if os.path.isdir( "./out/" ): shutil.rmtree( "./out/" )
+    if os.path.isdir( "./out/" ): shutil.rmtree( "./out/" )
     
     for top, n in TOPOLOGY:
         for lay in LAYERS:
@@ -26,7 +26,7 @@ if __name__ == "__main__":
                             TB = TemplateBuilder( SIMTYPE,l,t,lay,base/divisor,top,sec,n )
                             filepath = f"./out/res{c}"
                             c += 1
-                            #os.makedirs( filepath )
+                            os.makedirs( filepath )
                             TB.compile( filepath )
                             TB.writeConfigs( filepath )
     
